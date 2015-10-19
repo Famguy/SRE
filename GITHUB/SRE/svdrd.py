d79fce07f078d8770384d91b719cd94a20b9e93a
Sagun Pai <sagung.pai@gmail.com>
2015-10-19 15:37:10 +0530

    code done :D

--- GITHUB/SRE/svdrd.py
+++ GITHUB/SRE/svdrd.py
@@ -1,4 +1,19 @@
 import numpy as np
+# SVD-RD for Row Downdate
 
-
+def rowdowndate(U, S, V, k, q):
+# Input: U, S, V, k and q
+# k : Rank approximation given (one more than actual value) : int
+# q : Number of rows to be downdated (one more than actual value) : int
+# S : diagonal matrix containing top k singular values of A : numpy array
+# U, V : corresponding left and right singular vectors of A : numpy arrays
+# A = USVt 
+	D = U[m-q+1:m,1:k]
+	C = np.dot(D,S)
+	B = S-np.dot(D.T,C)
+	X, Y, Z = np.linalg.svd(B, full_matrices=True)
+	U1 = np.dot(U[1:m-q,1:k],X[:,1:k])
+	S1 = Y[1:k,1:k]
+	V1 = np.dot(V,Z[:,1:k])
+	return U1, S1, V1
 
